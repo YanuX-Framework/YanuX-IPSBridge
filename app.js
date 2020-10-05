8099df431d282e7c5ea0eedb59c2c5bee1f1d7d3
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-10-05 01:18:04 +0100

    - Working on integrating the Indoor App Server Proximity events with the YanuX Broker

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const ServerConnection = require('./src/ServerConnection');
+const io = require('socket.io-client');
+const feathers = require('@feathersjs/feathers');
+const socketio = require('@feathersjs/socketio-client');
+const auth = require('@feathersjs/authentication-client');
 
-const conn = new ServerConnection('wss://indoorlocationapp.herokuapp.com/ws/');
+const IndoorAppServerConnection = require('./src/IndoorAppServerConnection');
 
-console.log('YanuX IPS Bridge');
+const INDOORAPP_SERVER_URI = 'wss://indoorlocationapp.herokuapp.com/ws/';
+const INDOORAPP_SERVER_REALM = 'realm1';
+const YANUX_BROKER_URI = 'http://localhost:3002/';
+const INACTIVE_LOCATIONS_TIMEOUT = 12000;
 
-conn.connect();
+const main = async () => {
+    console.log('YanuX IPS Bridge');
+
+    const socket = io(YANUX_BROKER_URI);
+    const client = feathers();
+    client.configure(socketio(socket));
+    client.configure(auth());
+
+    try {
+        await client.authenticate({
+            strategy: 'local',
+            email: 'admin@yanux.org',
+            password: 'admin'
+        });
+        const locationService = client.service('location');
+        const conn = new IndoorAppServerConnection(
+            INDOORAPP_SERVER_URI,
+            INDOORAPP_SERVER_REALM,
+            locationService,
+            INACTIVE_LOCATIONS_TIMEOUT
+        );
+        conn.connect();
+    } catch (e) { console.error('Error:', e); }
+}
+
+main();
---
+++ src/IndoorAppServerConnection.js
@@ -0,0 +1,78 @@
+const util = require('util');
+const autobahn = require('autobahn');
+const { throws } = require('assert');
+
+module.exports = class IndoorAppServerConnection {
+    constructor(url, realm, locationService, inactiveLocationsTimeout = 5000) {
+        this.url = url;
+        this.realm = realm;
+        this.locationService = locationService;
+        this.connection = new autobahn.Connection({ url: this.url, realm: this.realm });
+        this.inactiveLocationsTimeout = inactiveLocationsTimeout;
+        this.inactiveLocationsTimer = null;
+        this.beaconRegex = /([ABCDEF0123456789]+\-[ABCDEF0123456789]+\-[ABCDEF0123456789]+\-[ABCDEF0123456789]+\-[ABCDEF0123456789]+)\-(\d+)\-(\d+)/i
+        this.connection.onopen = session => {
+            console.log('Connected to Indoor App Server');
+            session.subscribe('onLocationUpdate', async locationUpdates => {
+                console.log('> onLocationUpdate:', util.inspect(locationUpdates, false, null, true));
+                const locations = locationUpdates.map(locationUpdate => {
+                    const location = {
+                        username: locationUpdate.username,
+                        deviceUuid: locationUpdate.UUID,
+                        timestamp: new Date()
+                    };
+                    //Preparing location object with proximity information.
+                    if (locationUpdate.position) {
+                        location.proximity = {
+                            distance: locationUpdate.position.Regression,
+                            zone: locationUpdate.position.Classification
+                        };
+                        if (locationUpdate.beacon) {
+                            const beaconMatches = locationUpdate.beacon.match(this.beaconRegex);
+                            if (beaconMatches.length === 4) {
+                                location.proximity.beacon = {
+                                    uuid: beaconMatches[1],
+                                    major: beaconMatches[2],
+                                    minor: beaconMatches[3]
+                                };
+                            }
+                        }
+                    }
+                    //TODO: Prepare location object with positioning information based on fingerprinting or trilateration.
+                    return location;
+                });
+
+                console.log('> locations:', util.inspect(locations, false, null, true));
+                try {
+                    const locationUpdatesResults = await Promise.all(
+                        locations.map(location => {
+                            const query = {
+                                username: location.username,
+                                deviceUuid: location.deviceUuid
+                            }
+                            if (location.proximity && location.proximity.beacon) {
+                                query['proximity.beacon.uuid'] = location.proximity.beacon.uuid;
+                                query['proximity.beacon.major'] = location.proximity.beacon.major;
+                                query['proximity.beacon.minor'] = location.proximity.beacon.minor;
+                            }
+                            return this.locationService.patch(null, location, { query })
+                        })
+                    );
+                    console.log('>> Updated Locations:', util.inspect(locationUpdatesResults, false, null, true));
+                } catch (e) { console.log('>> Error Updating Locations:', e); }
+
+                clearTimeout(this.inactiveLocationsTimer)
+                this.inactiveLocationsTimer = setTimeout(async () => {
+                    console.log('>> Clearing Inactive Locations...');
+                    try {
+                        const removedInactiveLocations = await this.locationService.remove(null,
+                            { query: { timestamp: { $lt: new Date().getTime() - this.inactiveLocationsTimeout } } }
+                        );
+                        console.log('>>> Removed Inactive Locations:', util.inspect(removedInactiveLocations, false, null, true));
+                    } catch (e) { console.error('>> Error Removing Inactive Locations:', e); }
+                }, this.inactiveLocationsTimeout);
+            });
+        };
+    }
+    connect() { this.connection.open(); }
+}
