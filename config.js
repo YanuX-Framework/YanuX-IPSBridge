bd57b631331f7b28269198ce9f5eb5941e70a120
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-10-30 01:02:14 +0000

    - Changed default config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     "indoorapp_server": {
-        "url": process.env.INDOORAPP_SERVER_URL || "wss://yanux-ipsserver.herokuapp.com/ws/",
+        "url": process.env.INDOORAPP_SERVER_URL || "wss://indoorlocationapp.herokuapp.com/ws/",
         "realm": process.env.INDOORAPP_SERVER_REALM || "realm1"
     },
     "yanux_broker": {
-        "url": process.env.YANUX_BROKER_URL || "https://yanux-broker.herokuapp.com/",
+        "url": process.env.YANUX_BROKER_URL || "https://localhost:3002/",
         "authentication": {
             "strategy": process.env.YANUX_BROKER_AUTHENTICATION_STRATEGY || "local",
             "email": process.env.YANUX_BROKER_AUTHENTICATION_EMAIL || "admin@yanux.org",
